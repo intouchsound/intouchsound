ac335b982d2269e645408893311b0f124fe40b6d
markelite <mikhailovm123@gmail.com>
2024-07-24 16:06:51 +0800

    Revert changes after 0f90b5f3116a5da724e0fa2286b1a44156bea550

--- script.js
+++ script.js
@@ -1,65 +1,3 @@
-const handPositions = {"desktop": {"start": 45, "normal": 15, "hover": 1, "click1": 4, "click2": 4},
-    "mobile": {"start": 55, "normal": 3, "hover": 1, "click1": 4, "click2": 4}}
-
-function updateHandPosition(action) {
-    const viewportWidth = window.innerWidth;
-    if (action === "away") {
-        if (viewportWidth < 1024) {
-            leftHand.style.transform = `translateX(-${handPositions.mobile.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.mobile.normal}vw)`;
-        } else {
-            leftHand.style.transform = `translateX(-${handPositions.desktop.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.desktop.normal}vw)`;;
-        }
-    }
-    else if (action === "over") {
-        if (viewportWidth < 1024) {
-            leftHand.style.transform = `translateX(${handPositions.mobile.hover - handPositions.mobile.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.mobile.normal - handPositions.mobile.hover}vw)`;
-        } else {
-            leftHand.style.transform = `translateX(${handPositions.desktop.hover - handPositions.desktop.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.desktop.normal - handPositions.desktop.hover}vw)`;
-        }
-    }
-    else if (action === "click1") {
-        //this means button was hovered over
-        if (viewportWidth < 1024) {
-            // For mobile devices, adjust hand position to not roll out completely
-            leftHand.style.transform = `translateX(${handPositions.mobile.click1 - handPositions.mobile.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.mobile.normal - handPositions.mobile.click1}vw)`;
-        } else {
-            // For desktop, restore original extended position
-            leftHand.style.transform = `translateX(${handPositions.desktop.click1 - handPositions.desktop.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.desktop.normal - handPositions.desktop.click1}vw)`;
-        }
-    }
-    else if (action === "click2") {
-        //this means button was hovered over
-        if (viewportWidth < 1024) {
-            // For mobile devices, adjust hand position to not roll out completely
-            leftHand.style.transform = `translateX(${handPositions.mobile.click2 - handPositions.mobile.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.mobile.normal - handPositions.mobile.click2}vw)`;
-        } else {
-            // For desktop, restore original extended position
-            leftHand.style.transform = `translateX(${handPositions.desktop.click2 - handPositions.desktop.normal}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.desktop.normal - handPositions.desktop.click2}vw)`;
-        }
-    }
-    else if (action === "start") {
-        //this means button was hovered over
-        if (viewportWidth < 1024) {
-            // For mobile devices, adjust hand position to not roll out completely
-            leftHand.style.transform = `translateX(-${handPositions.mobile.start}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.mobile.start}vw)`;
-        } else {
-            // For desktop, restore original extended position
-            leftHand.style.transform = `translateX(-${handPositions.desktop.start}vw)`;
-            rightHand.style.transform = `translateX(${handPositions.desktop.start}vw)`;
-        }
-    }
-
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     const button = document.querySelector('.call-to-action-button');
     const leftHand = document.querySelector('.left-hand');
@@ -68,9 +6,75 @@ document.addEventListener("DOMContentLoaded", function() {
     const loadPercentage = document.getElementById('loadPercentage');
     const backgroundVideo = document.getElementById('backgroundVideo');
     const callToActionContainer = document.getElementById('callToActionContainer');
+    const callToActionButton = document.getElementById('callToActionButton');
     const duration = 2500;
+    var clickCount = 0;
 
+    const handPositions = {"desktop": {"start": 45, "normal": 15, "hover": 1, "click1": 4, "click2": 4},
+                            "mobile": {"start": 55, "normal": 3, "hover": 1, "click1": 4, "click2": 4}}
 
+    function updateHandPosition(action) {
+        const viewportWidth = window.innerWidth;
+        if (action === "away") {
+            //this means that mouse was moved away
+            if (viewportWidth < 1024) {
+                leftHand.style.transform = `translateX(-${handPositions.mobile.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.mobile.normal}vw)`;
+            } else {
+                leftHand.style.transform = `translateX(-${handPositions.desktop.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.desktop.normal}vw)`;;
+            }
+        }
+        else if (action === "over") {
+            //this means button was hovered over
+            if (viewportWidth < 1024) {
+                // For mobile devices, adjust hand position to not roll out completely
+                leftHand.style.transform = `translateX(${handPositions.mobile.hover - handPositions.mobile.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.mobile.normal - handPositions.mobile.hover}vw)`;
+            } else {
+                // For desktop, restore original extended position
+                leftHand.style.transform = `translateX(${handPositions.desktop.hover - handPositions.desktop.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.desktop.normal - handPositions.desktop.hover}vw)`;
+            }
+        }
+        else if (action === "click1") {
+            //this means button was hovered over
+            if (viewportWidth < 1024) {
+                // For mobile devices, adjust hand position to not roll out completely
+                leftHand.style.transform = `translateX(${handPositions.mobile.click1 - handPositions.mobile.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.mobile.normal - handPositions.mobile.click1}vw)`;
+            } else {
+                // For desktop, restore original extended position
+                leftHand.style.transform = `translateX(${handPositions.desktop.click1 - handPositions.desktop.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.desktop.normal - handPositions.desktop.click1}vw)`;
+            }
+        }
+        else if (action === "click2") {
+            //this means button was hovered over
+            if (viewportWidth < 1024) {
+                // For mobile devices, adjust hand position to not roll out completely
+                leftHand.style.transform = `translateX(${handPositions.mobile.click2 - handPositions.mobile.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.mobile.normal - handPositions.mobile.click2}vw)`;
+            } else {
+                // For desktop, restore original extended position
+                leftHand.style.transform = `translateX(${handPositions.desktop.click2 - handPositions.desktop.normal}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.desktop.normal - handPositions.desktop.click2}vw)`;
+            }
+        }
+        else if (action === "start") {
+            //this means button was hovered over
+            if (viewportWidth < 1024) {
+                // For mobile devices, adjust hand position to not roll out completely
+                leftHand.style.transform = `translateX(-${handPositions.mobile.start}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.mobile.start}vw)`;
+            } else {
+                // For desktop, restore original extended position
+                leftHand.style.transform = `translateX(-${handPositions.desktop.start}vw)`;
+                rightHand.style.transform = `translateX(${handPositions.desktop.start}vw)`;
+            }
+        }
+
+    }
 
     function incrementPercentage(element, targetPercentage, updateInterval) {
         let currentPercentage = parseInt(element.textContent) || 0; // Parse the current percentage or default to 0
@@ -96,20 +100,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     let start = null;
-    var handsContainer = document.getElementById('handsContainer');
     function step(timestamp) {
-        if (!start) {
-            start = timestamp;
-        }
+        if (!start) start = timestamp;
         const elapsed = timestamp - start;
 
         if (elapsed < duration) {
             const progress = elapsed / duration;
             loadPercentage.textContent = Math.min(Math.round(progress * progress * 100), 100) + "%";
-            if (loadPercentage.textContent === "50%") {
-                handsContainer.style.transition = 'opacity 2s ease-in-out;'
-                handsContainer.style.opacity = '1';
-            }
             window.requestAnimationFrame(step);
         } else {
             loadPercentage.textContent = '100%';
@@ -120,12 +117,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function endLoading() {
         overlay.style.opacity = '0';
-        handsContainer.style.opacity = '1';
 
         setTimeout(() => {
             overlay.style.display = 'none';
             overlay.style.zIndex = '-1';
             callToActionContainer.style.zIndex = '10000';
+            // loadPercentage.style.opacity = '0'
         }, 1000);
     }
 
@@ -150,30 +147,41 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 });
 
+// document.addEventListener("DOMContentLoaded", function() {
+//     const video = document.getElementById('backgroundVideo');
+//     const image = document.getElementById('backgroundImage');
+//
+//     video.addEventListener('error', function() {
+//         video.style.display = 'none';
+//         image.style.display = 'block';  // Show the image if there is an error with the video
+//     });
+//
+//     video.addEventListener('canplay', function() {
+//         video.style.display = 'block';
+//         image.style.display = 'none';  // Hide the image if the video can play
+//     });
+//
+//     // Check if video has enough data to play
+//     if (video.readyState < 3) {
+//         video.style.display = 'none';
+//         image.style.display = 'block';
+//     }
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById('backgroundVideo');
     var fallbackImage = document.getElementById('backgroundImage');
-    var handsContainer = document.getElementById('handsContainer');
-
-    function calculatePositionForWidthPercentage(percentage, isNegative) {
-        const position = (percentage / 100) * window.innerWidth;
-        return (isNegative ? -position : position) + 'px';
-    }
 
     // Attempt to play the video
     var playPromise = video.play();
 
     if (playPromise !== undefined) {
         playPromise.then(_ => {
-
+            // Video is playing, do nothing
         }).catch(error => {
-            video.style.display = 'none';
-            fallbackImage.style.display = 'block';
-            handsContainer.style.opacity = '0'
-            console.log(handsContainer)
-            setTimeout(() => {
-            }, 1000);
-
+            // Video failed to play, show fallback image
+            video.style.display = 'none'; // Hide the video
+            fallbackImage.style.display = 'block'; // Show the fallback image
         });
     }
 });
